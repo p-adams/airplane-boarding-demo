f3c3cecd6ed84e517dd9e96bba50e72417e074cf
Paul Adams <ghusiyy@gmail.com>
2022-10-25 23:53:47 -0700

    add seat chart component

---
+++ src/lib/SeatChart/index.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+</script>
+
+<div class="Seat-chart">
+  <div class="Port">
+    <div>22</div>
+    <div>23</div>
+    <div>24</div>
+    <div>25</div>
+    <div>26</div>
+    <div>27</div>
+    <div>28</div>
+    <div>29</div>
+    <div>30</div>
+    <div>31</div>
+    <div>32</div>
+    <div>34</div>
+    <div>35</div>
+    <div>36</div>
+    <div>37</div>
+    <div>38</div>
+  </div>
+  <div class="Starboard">
+    <div>22</div>
+    <div>23</div>
+    <div>24</div>
+    <div>25</div>
+    <div>26</div>
+    <div>27</div>
+    <div>28</div>
+    <div>29</div>
+    <div>30</div>
+    <div>31</div>
+    <div>32</div>
+    <div>34</div>
+    <div>35</div>
+    <div>36</div>
+    <div>37</div>
+    <div>38</div>
+  </div>
+</div>
+
+<style>
+  .Seat-chart {
+    height: 600px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+</style>
